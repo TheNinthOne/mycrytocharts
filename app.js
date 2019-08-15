406b85e96d40ea902900f9609946d4145262d8a6
TheNinthOne <ivkovicolga@gmail.com>
2019-08-15 15:07:17 +0200

    Added loging and database access | Update #3 |

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
+const fs = require("fs");
 const path = require("path");
 const morgan = require("morgan");
 const express = require("express");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const { USER, PASSWORD, DATABASE, HOST } = process.env;
+const connectionStr = `mongodb://${USER}:${PASSWORD}@${HOST}:27017/${DATABASE}`;
+
+mongoose.connect(connectionStr, {
+  useNewUrlParser: true
+});
+mongoose.set("useCreateIndex", true);
+mongoose.set("useFindAndModify", true);
+
+const db = mongoose.connection;
+
+db.on("error", error => {
+  console.error(error.message);
+  process.exit(0);
+});
+
+db.once("open", async () => {
+  console.log("Successfuly connected to MongoDB");
+});
 
 //Middleware includes
 const errorMiddleware = require("./api/middleware/error");
@@ -12,11 +36,19 @@ const hbsEngine = require("./handlebars/engine");
 const pageRouter = require("./api/routes/pages");
 const dataRouter = require("./api/routes/data");
 
+// create a write stream (in append mode)
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  {
+    flags: "a"
+  }
+);
+
 //Initialize app
 const app = express();
 
-//Dev middleware
-app.use(morgan("dev"));
+// setup the logger
+app.use(morgan("combined", { stream: accessLogStream }));
 
 //Set Handlebars as view engine
 app.engine("hbs", hbsEngine);
@@ -27,7 +59,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", pageRouter);
-app.use("/data", dataRouter);
+app.use("/api", dataRouter);
 
 //Including error handling middleware
 app.use(errorMiddleware.error_not_found);
